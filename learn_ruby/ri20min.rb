58fb570b315646f6eb213911fe2d589f4ef13804
TheGoldKay <jhonnatanwfc@live.com>
2024-02-11 10:22:51 -0300

    more classes

---
+++ learn_ruby/ri20min.rb
@@ -0,0 +1,63 @@
+#!/usr/share/rvm/rubies/ruby-3.0.0/bin/ruby
+
+class MegaGreeter
+    attr_accessor :names
+  
+    # Create the object
+    def initialize(names = "World")
+      @names = names
+    end
+  
+    # Say hi to everybody
+    def say_hi
+      if @names.nil?
+        puts "..."
+      elsif @names.respond_to?("each")
+        # @names is a list of some kind, iterate!
+        @names.each do |name|
+          puts "Hello #{name}!"
+        end
+      else
+        puts "Hello #{@names}!"
+      end
+    end
+  
+    # Say bye to everybody
+    def say_bye
+      if @names.nil?
+        puts "..."
+      elsif @names.respond_to?("join")
+        # Join the list elements with commas
+        puts "Goodbye #{@names.join(", ")}.  Come back soon!"
+      else
+        puts "Goodbye #{@names}.  Come back soon!"
+      end
+    end
+  end
+  
+  # __FILE__ holds the current file name
+  # $0 is the name of the file used to start the program
+  # if the main file is being used then run this, otherwise if 
+  # being required ignore these lines, this allows a file to serve as
+  # a module and also executable on its own
+  if __FILE__ == $0
+    mg = MegaGreeter.new
+    mg.say_hi
+    mg.say_bye
+  
+    # Change name to be "Zeke"
+    mg.names = "Zeke"
+    mg.say_hi
+    mg.say_bye
+  
+    # Change the name to an array of names
+    mg.names = ["Albert", "Brenda", "Charles",
+                "Dave", "Engelbert"]
+    mg.say_hi
+    mg.say_bye
+  
+    # Change to nil
+    mg.names = nil
+    mg.say_hi
+    mg.say_bye
+  end
